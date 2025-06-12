ef5480fbd0e3b771d7b68d2413884cc4e5267227
Louis Remes <>
2025-06-12 12:31:31 +0200

    test get file info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,12 +1,16 @@
 import os
 
 def get_files_info(working_directory, directory=None):
-    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)):
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
+    if not full_path.startswith(os.path.abspath(working_directory)):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    if not os.path.isdir(directory):
+    if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
     
     try:
-        return "\n".join([f"{content}: file_size={os.path.getsize(content)}, is_dir={os.path.isdir(content)}" for content in os.listdir(directory)])
+        return "\n".join([
+            f"{content}: file_size={os.path.getsize(os.path.join(full_path, content))}, is_dir={os.path.isdir(os.path.join(full_path, content))}"
+            for content in os.listdir(full_path)
+        ])
     except Exception as e:
         print(f"Error: {e.__class__.__name__}: {e}")
